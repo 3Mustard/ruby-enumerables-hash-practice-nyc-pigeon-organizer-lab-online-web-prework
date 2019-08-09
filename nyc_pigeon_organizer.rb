97457f7c6ac74c00c1983078ef204db5c20fe120
Matthew C <m23.cullen@gmail.com>
2019-08-09 20:08:59 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -10,5 +10,8 @@ def nyc_pigeon_organizer(data)
   pigeons_names = pigeons.keys
   data[:color].each do |color,names|
     names.each do |name|
+      pigeons_names.each do |pigeon_name|
+        if pigeon_name == name
+          
       
 end
